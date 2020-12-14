c66a56c5f8b1f821322ca7cc9b6655bc26580dfb
Mateusz Gromadzki <mateusz079@wp.pl>
2020-12-15 00:22:58 +0100

    Add Card component

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -1,15 +1,48 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './Column.scss';
+import Card from '../Card/Card.js';
+import {settings,} from '../../data/dataStore';
+import Creator from '../Creator/Creator.js';
+import Icon from '../Icon/Icon.js';
 
 class Column extends React.Component{
+  state = {
+    cards: this.props.cards || [],
+    
+  }
   static propTypes = {
     titleColumn: PropTypes.string,
   }
+
+  addCard(title){
+    this.setState(state => (
+      {
+        cards: [
+          ...state.cards,
+          {
+            key: state.cards.length ? state.cards[state.cards.length-1].key+1 : 0,
+            title,
+          }
+        ]
+      }
+    ));
+  }
+
   render() {
     return (
       <section className={styles.component}>
-        <h3 className={styles.title}>{this.props.titleColumn}</h3>
+        <h3 className={styles.title}><span className={styles.icon}><Icon name={this.props.icon} /></span>{this.props.title}</h3>
+
+        <div>
+          {this.state.cards.map(({key, ...cardProps}) => (
+            <Card key={key} {...cardProps} />
+          ))}
+        </div>
+        <div className={styles.creator}>
+          <Creator text={settings.cardCreatorText} action={title => this.addCard      (title)}/>
+        </div>
+
       </section>
     )
   }
