6768f37a61103b8b51876dc8f33910779081a285
Mateusz Gromadzki <mateusz079@wp.pl>
2020-12-20 22:03:27 +0100

    Add adding columns redux

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './Column.scss';
 import Card from '../Card/Card.js';
-//import {settings} from '../../data/dataStore';
+import {settings} from '../../data/dataStore';
 //import Creator from '../Creator/Creator.js';
 import Icon from '../Icon/Icon.js';
 
@@ -11,6 +11,10 @@ class Column extends React.Component{
     title: PropTypes.string,
     cards: PropTypes.array,
     icon: PropTypes.string,
+  }
+
+  static defaultProps = {
+    icon: settings.defaultColumnIcon,
   }
 
   render() {
