4034d316f769216d24299fe19143a816b888ccb5
Mateusz Gromadzki <mateusz079@wp.pl>
2020-12-20 23:15:07 +0100

    Add adding cards redux

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import styles from './Column.scss';
 import Card from '../Card/Card.js';
 import {settings} from '../../data/dataStore';
-//import Creator from '../Creator/Creator.js';
+import Creator from '../Creator/Creator.js';
 import Icon from '../Icon/Icon.js';
 
 class Column extends React.Component{
@@ -11,6 +11,7 @@ class Column extends React.Component{
     title: PropTypes.string,
     cards: PropTypes.array,
     icon: PropTypes.string,
+    addCard: PropTypes.func,
   }
 
   static defaultProps = {
@@ -18,7 +19,7 @@ class Column extends React.Component{
   }
 
   render() {
-    const {title, icon, cards} = this.props;
+    const {title, icon, cards, addCard} = this.props;
     return (
       <section className={styles.component}>
         <h3 className={styles.title}><span className={styles.icon}><Icon name={icon} /></span>{title}</h3>
@@ -28,11 +29,10 @@ class Column extends React.Component{
             <Card key={cardData.id} {...cardData} />
           ))}
         </div>
-        {/*
         <div className={styles.creator}>
-          <Creator text={settings.cardCreatorText} action={title => this.addCard      (title)}/>
-          </div>
-        */}
+          <Creator text={settings.cardCreatorText} action={addCard}/>
+        </div>
+  
 
       </section>
     );
